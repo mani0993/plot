c6bd09bfa00150e17cb27517d456a7c8e95e787a
mani0993 <mani0993@gmail.com>
2024-09-03 22:05:58 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PineScriptEditor from './PineScriptEditor';
+
+const App = () => {
+    return (
+        <div>
+            <PineScriptEditor />
+        </div>
+    );
+};
+
+export default App;
